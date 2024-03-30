278b30870f8b01bb6a31df3ab3677e325de3392e
Yeonseung Yoo <85042907+Yonn-Yoo@users.noreply.github.com>
2024-03-30 23:33:29 +0900

    feat: create appbar widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,8 +6,21 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text('오늘의 웹툰'),
+        elevation: 3,
+        surfaceTintColor: Colors.white,
+        shadowColor: Colors.black,
+        backgroundColor: Colors.white,
+        title: const Text(
+          '오늘의 웹툰',
+          style: TextStyle(
+            letterSpacing: 1.4,
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+            color: Colors.green,
+          ),
+        ),
       ),
     );
   }
